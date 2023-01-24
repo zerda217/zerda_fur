d256e07c7e8c8c61d4710ea755408b4056c1063d
zerda217 <zerda@korea.ac.kr>
2023-01-24 12:52:05 +0900

    투두리스트 등록

--- src/pages/Todos/todos.js
+++ src/pages/Todos/todos.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
+import { useSelector } from "react-redux";
 import styled from 'styled-components'
 
 const GET_TODOS = gql`
@@ -8,27 +9,62 @@ const GET_TODOS = gql`
   }
 `
 
+const ADD_TODO = gql`
+  mutation newPost($post: TodoInput) {
+  newPost(post: $post)
+}
+`
+
 const Todos = () => {
   const [addTodo, setAddTodo] = useState('');
+  const [addCategory, setAddCategory] = useState('인간');
+  const category = ['인간', '고양이', '투두']
 
-  const {data} = useQuery(GET_TODOS);
+  const { id } = useSelector((state) => state.user);
+  const { data, refetch } = useQuery(GET_TODOS);
 
   const onChange = (e) => {
     setAddTodo(e.target.value)
-  }
+  };
+
+  const onClickAddTodo = () => {
+    newPost({
+      variables: {
+        post: {
+          title: addTodo,
+          register_user: id === 0 && 1,
+          category: addCategory
+        }
+      }
+    })
+  };
+
+  const NewPostCompleted = () => {
+    setAddTodo('')
+    setAddCategory('인간')
+    refetch()
+  };
+
+  const [newPost] = useMutation(ADD_TODO, {
+    onCompleted: NewPostCompleted
+  });
 
   return (
     <Wrap>
       <h2>투두리스트 ({data?.todoList.todoList.length})</h2>
-
-      <StyledInput
-        type='text'
-        name='addTodo'
-        value={addTodo}
-        placeholder='할 일 추가'
-        onChange={onChange}
-      />
-      <button> 추가 </button>
+      <StyledAddTodo>
+        <select name="category" onClick={(e) => setAddCategory(e.target.value)}>
+          {category.map((i) => <option value={i}>{i}</option>)}
+        </select>
+        <StyledInput
+          type='text'
+          name='addTodo'
+          value={addTodo}
+          placeholder='할 일 추가'
+          onChange={onChange}
+        />
+        <StyledButton onClick={() => onClickAddTodo()}> 추가 </StyledButton>
+      </StyledAddTodo>
 
       {data?.todoList.todoList.map((todo) => 
         <StyledList key={todo.id}>
@@ -50,12 +86,30 @@ const Wrap = styled.div`
   margin: 1vh;
 `;
 
+const StyledAddTodo = styled.div`
+  display: flex;
+  margin: 0 0 2vh;
+  padding: 0 0 2vh;
+  width: 80%;
+`
+
 const StyledInput = styled.input`
   padding: 1vh;
-  margin: 1vh;
+  margin: 0 1vh;
   border: 2px solid #888;
-  width: 50vh;
+  width: 80%;
   font-size: 2vh;
+`
+
+const StyledButton = styled.button`
+  background: #888;
+  border: none;
+  color: #fff;
+  width: 10%;
+
+  &:hover {
+    background: #880217;
+  }
 `
 
 const StyledList = styled.div`
