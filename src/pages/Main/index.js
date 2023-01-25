74194b1a56a6ce2f391bfa723fe96130d51b67fb
zerda217 <zerda@korea.ac.kr>
2023-01-25 14:38:11 +0900

    no message

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -8,12 +8,10 @@ const Index = () => {
   return (
     <Wrap>
       <Completed>
-
+        <h2>BACK : https://github.com/zerda217/zerda_sand</h2>
+        <h2>FRONT : https://github.com/zerda217/zerda_fur</h2>
       </Completed>
 
-      <Todo>
-
-      </Todo>
       <IconDiv>
         <GoMarkGithub size='5vh' onClick={() => window.open('https://github.com/zerda217', '_blank')} />
         <SiNotion size='5vh' onClick={() => window.open('https://zerda217.notion.site/7a0a96d6494d47a597b9f5e2af45a807', '_blank')} />
@@ -35,13 +33,6 @@ const Completed = styled.div`
   height: 15vh;
   margin: 5% 0;
   background: #2dd;
-`
-
-const Todo = styled.div`
-  width: 100%;
-  height: 15vh;
-  margin: 5% 0;
-  background: #22ffaa;
 `
 
 const IconDiv = styled.div`
